22ca922ec6f1398540ddbae9ab69b0fb908f84b0
Leandro Loiola <leandrochataum@gmail.com>
2013-03-23 16:06:23 -0300

    Alteracao do data-required para contemplar validacoes para radio, checkbox e textarea

--- js/wdev-required.js
+++ js/wdev-required.js
@@ -3,16 +3,15 @@
  */
  
  $(document).ready(function(){
- 
 	/**
-	 * INSERIR DIV DE ERROS ANTES DO FORM
+	 * Adiciona uma div para a listagem dos erros de validação acima do form.
 	 */
 	 $("form").before("<div id='list-errors' style='display: none;'></div>");
 	
 	/**
-	 * INSERIR SINALIZAÇÃO NOS CAMPOS OBRIGATÓRIOS
+	 * Adiciona nos campos obrigatórios um * para indicar sua obrigatoriedade.
 	 */
-	$("input[data-required], select[data-required]").each(function(){
+	$("[data-required]").each(function(){
 		var input = $(this);
 		var required = input.attr('data-required');
 		var obrigatoriedade = "<span class='obrigatorio'>*</span>";
@@ -33,30 +32,36 @@
 	
 	
 	/**
-	 * CONTROLAR SE OS CAMPOS OBRIGATÓRIOS FORAM PREENCHIDOS
+	 * Verifica se os campos com o atributo data-required foram preenchidos.
 	 */
 	 $("form").submit(function(){
 		var hasError = false;
 		var listErrors = "";
 		var errors = $("div#list-errors");
 		
-		$("input[data-required], select[data-required]").each(function(){
+		$("[data-required]").each(function(){
 			var input = $(this);
+			var type = input.attr('type');
 			var required = input.attr('data-required');
 			var value = input.val();
 			
-			if(required != ""){	
-				if(value == "" || value == 0){
+			// Verifica o valor de campos do tipo Radio e Checkbox
+			if(type == 'radio' || type == 'checkbox') {
+				var checkedValue = $('input[name=' + input.attr('name') + ']:checked').val();
+				value = checkedValue == undefined ? '' : checkedValue;
+			}
+			
+			if(required != "") {
+				if((value == "" || value == 0) && listErrors.indexOf(required) < 1){
 					input.addClass('required');
-					listErrors += "<li>"+required+" é obrigatório</li>";
+					listErrors += "<li>" + required + " é obrigatório</li>";
 					hasError = true;
 				} else {
 					input.removeClass('required');
 				}
 			}
 		});
-		
-		
+			
 		if(hasError){
 			errors.show();
 			errors.html(listErrors);
